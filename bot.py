4ca5153fd364c2d590b6ddaee643daa2c3c7a240
marlenzhai-wq <marlen.zhai@gmail.com>
2026-06-30 02:39:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -327,7 +327,7 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
+    #await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
