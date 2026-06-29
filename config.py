231f2e0be061bae492758cdacdbb764d4c1bbcef
marlenzhai-wq <marlen.zhai@gmail.com>
2026-06-30 02:58:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # Токенді осында жазыңыз немесе BOT_TOKEN env переменнойы арқылы беріңіз
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8864955547:AAH5sCdKM-vEypEFPjox5MyNh51L2LdI774")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8864955547:AAGZ48_baItmVVVuy-mspVLI_rs_hmcVW3E")
 
 DB_PATH = os.getenv("BINGO_DB_PATH", "bingo.db")
